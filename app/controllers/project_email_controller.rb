a6c37c9cf038f16f5a597632d68f03e3afda403b
Garrett Pauls <garrett.pauls@gmail.com>
2012-03-01 20:05:11 -0700

    Added support for saving emails.

--- app/controllers/project_email_controller.rb
+++ app/controllers/project_email_controller.rb
@@ -17,11 +17,11 @@ class ProjectEmailController < ApplicationController
       :project_id => @project.id,
       :id => params[:email_id] }
     if not @email then
-      @email = ProjectEmail.new :project => @project, :sender => User.current
+      @email = ProjectEmail.new :project => @project, :sender => User.current, :sent => false
     end
 
     @available_recipients = @project.users.map do |user|
-      existing = @email.recipients.first {|r| r.user_id == user.id}
+      existing = @email.recipients.first :conditions => { :user_id => user.id }
       r = OpenStruct.new
       r.user_id = user.id
       r.name = user.name
@@ -36,6 +36,69 @@ class ProjectEmailController < ApplicationController
         r.bcc = false
       end
       r
+    end
+  end
+
+  def save
+    _load_and_save_email_from_compose
+    if @email then
+      if params['send'] then
+        send_email
+      else
+        flash[:notice] = t(:message_draft_saved)
+        redirect_to :action => 'compose', :project_id => @email.project.id, :email_id => @email.id
+      end
+    else
+      redirect_to :action => 'index', :project_id => params[:project_id]
+    end
+  end
+
+  def send_email
+    flash[:notice] = 'TODO: Send email'
+    redirect_to :action => 'index', :project_id => @email.project.id
+  end
+
+  def _load_and_save_email_from_compose
+    emailp = params['project_email']
+    if emailp then
+      @email =
+        begin
+          ProjectEmail.find emailp['id']
+        rescue
+          ProjectEmail.new :sender => User.current, :sent => false, :project => (Project.find emailp['project_id'])
+        end
+
+      @email.project_id = emailp['project_id']
+      @email.subject = emailp['subject']
+      @email.body = emailp['body']
+
+      recips = []
+
+      if emailp['recipients'] then
+        emailp['recipients'].each do |r, opts|
+          recip =
+            begin
+              @email.recipients.find :user_id => r.to_i
+            rescue
+              ProjectEmailRecipient.new :user_id => r.to_i, :project_email => @email
+            end
+
+          recip.to = opts.keys.include?('to')
+          recip.cc = opts.keys.include?('cc')
+          recip.bcc = opts.keys.include?('bcc')
+          recip.save
+          recips << recip
+        end
+      end
+
+      removed = @email.recipients - recips
+      removed.each do |r|
+        r.delete
+      end
+
+      @email.save
+    else
+      @email = nil
     end
   end
 end
--- app/models/project_email_recipient.rb
+++ app/models/project_email_recipient.rb
@@ -3,4 +3,13 @@ class ProjectEmailRecipient < ActiveRecord::Base
 
   belongs_to :project_email
   belongs_to :user
+
+  def to_s
+    toccbcc = ''
+    toccbcc << 'to, ' if to
+    toccbcc << 'cc, ' if cc
+    toccbcc << 'bcc, ' if bcc
+
+    "#{user.name}: #{toccbcc}#{project_email.subject}"
+  end
 end
