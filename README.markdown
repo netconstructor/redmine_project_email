b9cb5b1e2c00e0540793617e77bb94d3faf2b42b
Garrett Pauls <garrett.pauls@gmail.com>
2012-03-03 15:13:16 -0700

    Updated README and project url.

---
+++ README.markdown
@@ -0,0 +1,6 @@
+Redmine Project Email
+---------------------
+
+This plugin allows users to send email to other users working in a project.
+
+
--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_project_email do
   author 'Garrett Pauls'
   description 'A plugin for sending email to project members'
   version '0.0.1'
-  # url ''
+  url 'https://github.com/garrettpauls/redmine_project_email'
 
   permission :project_email, { :project_email => [:index] }, :public => true
   menu :project_menu, :project_email, { :controller => :project_email, :action => :index }, :caption => :label_email, :after => :activity, :param => :project_id
