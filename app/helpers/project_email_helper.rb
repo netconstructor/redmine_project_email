1af0510dd4926e901f00906a8b28ecc3d2981fd9
Garrett Pauls <garrett.pauls@gmail.com>
2012-03-02 21:55:46 -0700

    Added sent date to email view.

--- app/helpers/project_email_helper.rb
+++ app/helpers/project_email_helper.rb
@@ -2,4 +2,8 @@ module ProjectEmailHelper
   def le(*args)
     html_escape l(*args)
   end
+  def ld(dt, options={})
+    format = if options[:format] then options[:format].to_s else 'default' end
+    dt.strftime(I18n.t(:"time.formats.#{format}", {:locale => I18n.locale}))
+  end
 end
